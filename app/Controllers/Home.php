e3ae8e39b56141a260d67954741b55a1edddb4c9
Gary Vee Reyes <reyesgaryvee@gmail.com>
2025-08-22 00:18:37 -0900

    Added Home controller, routes, and views for basic navigation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,19 +6,19 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        // Load homepage template
-        return view('template', ['content' => 'index']);
+        // Load homepage inside template
+        return view('template', ['content' => view('index')]);
     }
 
     public function about(): string
     {
-        // Load about page template
-        return view('template', ['content' => 'about']);
+        // Load about page inside template
+        return view('template', ['content' => view('about')]);
     }
 
     public function contact(): string
     {
-        // Load contact page template
-        return view('template', ['content' => 'contact']);
+        // Load contact page inside template
+        return view('template', ['content' => view('contact')]);
     }
 }
